cf6739a9d994a161d13273d4e6c1400a34b0f07c
Karen V <karenvo@usc.edu>
2019-12-12 02:36:32 +0000

    Automatically backed up by Learn

--- app/controllers/secrets_controller.rb
+++ app/controllers/secrets_controller.rb
@@ -1,6 +1,5 @@
 class SecretsController < ApplicationController
   before_action :require_login
-  skip_before_action :require_login, only: [:index]
 
   def show
     # redirect to /login if not logged in
