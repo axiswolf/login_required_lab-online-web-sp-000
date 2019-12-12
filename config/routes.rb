746f8e2470625d3ca385ec0c7ca3944c5145bdcb
Karen V <karenvo@usc.edu>
2019-12-12 02:43:23 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   get '/login' => 'sessions#new'
-  post '/login' => 'sessions#show'
-  # post '/login' => 'sessions#create'
+  # post '/login' => 'sessions#show'
+  post '/login' => 'sessions#create'
   post '/logout' => 'sessions#destroy'
 
   get '/secret' => 'secrets#show'
