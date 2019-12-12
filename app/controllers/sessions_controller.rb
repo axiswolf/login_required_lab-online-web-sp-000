c56d73aeab6ecb75598f8b42619224501c80b774
Karen V <karenvo@usc.edu>
2019-12-12 02:20:18 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,10 +1,13 @@
 class SessionsController < ApplicationController
 
   def create
+    if current_user.nil? || current_user.empty?
     # redirects to login page if :name is nil
     # redirects to login page if :name is empty
+      redirect_to '/login'
+    else 
     # sets session[:name] if :name is given
-
+    end
   end
 
   def destroy
