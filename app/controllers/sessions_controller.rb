dd5a6878f19376f7533277ea54196510918668d1
Karen V <karenvo@usc.edu>
2019-12-12 02:19:24 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,18 @@
 class SessionsController < ApplicationController
 
+  def create
+    # redirects to login page if :name is nil
+    # redirects to login page if :name is empty
+    # sets session[:name] if :name is given
+    
+  end
 
-
+  def destroy
+    if params[:name].nil?
+      session[:name] = params[:name]
+    else
+      session.destroy :name
+    end
+  end
+  
 end
